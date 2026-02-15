8a4e4e0d9608864029622a6c59831a763a2ce0e8
Gungun Agrawal <120359846+Gungunag1@users.noreply.github.com>
2026-02-15 09:06:07 +0530

    Enhance myPow function with comments
    Added comments for better understanding of edge cases and negative power handling.

--- Pow(x,n).cpp
+++ Pow(x,n).cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-
+      // Edge cases 
         if(n==0) return 1.0;
         if(x==0) return 0.0;
         if(x==1) return 1.0;
@@ -9,7 +9,7 @@ public:
         if(x==-1 && n%2!=0) return -1.0;
          long binform=n;
          double ans=1;
-
+ // To handle negative power 
          if(binform<0){
             x=1/x;
             binform=-binform;
@@ -24,4 +24,5 @@ public:
          }
          return ans;
     }
+
 };
